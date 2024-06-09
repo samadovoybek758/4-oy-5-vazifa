3ecf03df930be382da6bc3b6f34f54103e326060
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-09 10:23:55 +0500

    firstly

---
+++ js/index.js
@@ -0,0 +1,181 @@
+// break/continue
+// // 1-masala
+// for (let i = 1; i <= 50; i++) {
+//     if(i==30){
+//         break
+//     }
+//     console.log(i);
+// }
+
+// // 2-masala
+// for (let i = 1; i <= 50; i++) {
+//     if(i%5==0){
+//        continue
+//     }
+//     console.log(i);
+// }
+
+// // 3-masala
+// for (let i = 1; i <= 100; i++) {
+//     if(i==75){
+//         break
+//     }
+//     console.log(i);
+// }
+
+// // 4-masala
+// for (let i = 1; i <= 30; i++) {
+//     if(i%3==0){
+//        continue
+//     }
+//     console.log(i);
+// }
+
+// while and do while
+// 1-masala
+
+// let i = 1;
+// while (i<=20){
+//     console.log(i);
+//     i++
+// }
+
+// 2-masala
+// let i =10;
+// while (i>=1) {
+//     console.log(i);
+//     i--
+// }
+
+// 3-masala
+// let i =1;
+// while (i<=50) {
+//     if(i%2==0){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// 4-masala
+// let i =1;
+// while (i<=50) {
+//     if(i%2==1){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// 5-masala
+// let i =1;
+// let yig =0;
+// while(i<=100){
+
+//     yig= yig +i;
+//     i++
+// }
+// console.log(yig);
+
+// 6-masala
+// let i =1;
+// while (i<=10) {
+//     console.log(i**2);
+//     i++
+
+// }
+
+// 7-masala
+// let a = +prompt("sonni kiritng");
+// while (a>0) {
+
+//     let a = +prompt("sonni kiritng");
+
+//     if (a<0) {
+//         break
+//     }
+//     console.log(a);
+
+// }
+
+// 8-masala
+// let i =2;
+// while (i<=1024) {
+//     if (i==1024) {
+//         break
+//     }
+//     console.log(i);
+//     i*2;
+
+// }
+
+// 10-masala
+// let i=1;
+// while (i<=20) {
+//     if (i%3==0) {
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// 11-masala
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+// {
+// }
+
+
+// // 12-masala
+// let i = 20;
+// do {
+//   console.log(i);
+//   i--;
+// } while (i >= 1);
+// {
+// }
+
+
+
+// 13-masala
+// let i = 1;
+// do {
+//     if (i%2==0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 100);
+// {
+// }
+
+
+
+// 14-masala
+// let a = +prompt("sonni kiritng");
+
+// do{
+//     let a = +prompt("sonni kiritng");
+
+//     if (a==0) {
+//         break
+//     }
+//     console.log(a**2);
+// }
+// while (a>0) {
+// }
+
+
+
+// 15-masala
+// let a = +prompt("sonni kiritng");
+
+do{
+    let a = +prompt("sonni kiritng");
+
+    if (a==10) {
+        break
+    }
+    console.log(a);
+}
+while (a>0) {
+}
